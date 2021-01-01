115289e5c9220f399edb8616a7900602f24d956e
karukan013 <karukan029@gmail.com>
2021-01-01 21:56:34 +0900

    JSX

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
 
 function App() {
+  const list = ['Patty','Rolley', 'Bobby'];
   return (
+    <React.Fragment>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +23,12 @@ function App() {
         </a>
       </header>
     </div>
+    <ul>
+    {list.map((name) => (
+      <li>Hello, {name}!</li>
+    ))}
+  </ul>
+  </React.Fragment>
   );
 }
 
---
+++ src/components/Greets.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+type Props = {name: string; times? : number};
+// interface TProps {name: string; times? : number};
+
+const Greets: React.FunctionComponent<Props>= (props) => {
+    const { name, times = 1, children } = props;
+    // const tProps: TProps = {name: 'a',times: 5};
+    return ()
+}
