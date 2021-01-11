66a036ba1810eff65cb3f0f45bae5ccf05c1949a
karukan013 <karukan029@gmail.com>
2021-01-11 17:47:10 +0900

    component
    hooks
    State Hook_end

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -5,7 +5,14 @@ import './Counter.css';
 const Counter: FC = () => {
   const [count, setCount] = useState(0);
   const reset = () => setCount(0);
+  // hooks API useState stateに値を設定する関数の引数に関数を渡した場合、
+  // その関数の引数は直近のstate変数値で、戻り値が新たなstate変数値となる。
   const increment = () => setCount((c) => c + 1);
+  // const increment = () => setCount(count + 1);
+
+  // const plusThreeDirectly = () => [0, 1, 2].forEach((_) => setCount(count + 1));
+  // const plusThreeWithFunction = () =>
+  // [0, 1, 2].forEach((_) => setCount((c) => c + 1));
 
   return (
     <Card>
@@ -19,6 +26,7 @@ const Counter: FC = () => {
             Reset
           </Button>
           <Button color="green" onClick={increment}>
+            {/* <Button color="green" onClick={plusThreeDirectly}> */}
             +1
           </Button>
         </div>
