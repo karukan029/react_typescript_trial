eed80c7e5922806a9d9e00f439d56deeb0724e80
karukan013 <karukan029@gmail.com>
2021-01-11 16:01:12 +0900

    component
    hooks_start

--- src4/App.test.tsx
+++ src4/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const textElement = getByText(/カウンター/i);
+  const textElement = getByText(/タイマー/i);
   expect(textElement).toBeInTheDocument();
 });
