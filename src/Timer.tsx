c5e9fbabce0e5c965ebde2d89d327ee5bbd7aec6
karukan013 <karukan029@gmail.com>
2021-01-23 18:24:35 +0900

    hooks
    end_メモ化

--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,4 +1,11 @@
-import React, { FC, useEffect, useMemo, useCallback, useState } from 'react';
+import React, {
+  FC,
+  useEffect,
+  useMemo,
+  useCallback,
+  useRef,
+  useState,
+} from 'react';
 import { Button, Card, Icon, Statistic } from 'semantic-ui-react';
 import { getPrimes } from './utils/math-tools';
 import './Timer.css';
@@ -7,15 +14,26 @@ const Timer: FC<{ limit: number }> = ({ limit }) => {
   const [timeLeft, setTimeLeft] = useState(limit);
   // メモ化したファイルの呼び出し
   const primes = useMemo(() => getPrimes(limit), [limit]);
+  const timerId = useRef<NodeJS.Timeout>();
   // const reset = (): void => setTimeLeft(limit);
   const reset = useCallback(() => setTimeLeft(limit), [limit]);
-  const tick = (): void => setTimeLeft((t) => t - 1);
+  const tick = (): void => setTimeLeft((prevTime) => prevTime - 1);
 
   useEffect(() => {
-    const timerId = setInterval(tick, 1000);
+    // const timerId = setInterval(tick, 1000);
+    const clearTimer = () => {
+      if (timerId.current) clearInterval(timerId.current);
+    };
 
-    return () => clearInterval(timerId);
-  }, []);
+    reset();
+    clearTimer();
+    // timerIdをref属性にもつDOMにアクセス
+    timerId.current = setInterval(tick, 1000);
+
+    //   return () => clearInterval(timerId);
+    // }, []);
+    return clearTimer;
+  }, [limit, reset]);
 
   useEffect(() => {
     // if (timeLeft === 0) setTimeLeft(limit);
